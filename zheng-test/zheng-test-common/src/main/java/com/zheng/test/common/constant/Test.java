d745a945665c987c6f3334e92e85071c3611b7c9
liwei <904933131@qq.com>
2018-02-27 17:54:54 +0800

    删除QQ群

--- zheng-test/zheng-test-common/src/main/java/com/zheng/test/common/constant/Test.java
+++ zheng-test/zheng-test-common/src/main/java/com/zheng/test/common/constant/Test.java
@@ -29,5 +29,6 @@ package com.zheng.test.common.constant;
 public class Test {
     public static void main(String[] args) {
         System.out.println("项目第二期个人业务开发中。。。。");
+        System.out.println("开发了十来天 开发完毕 提交啦");
     }
 }
